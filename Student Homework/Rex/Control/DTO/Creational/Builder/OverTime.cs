8df3691aa6e1f5aa8476cc93943af2ab08cd317a
rexlucien <rexlucien@hotmail.com>
2015-11-28 00:36:05 +0800

    Adjust

--- Student Homework/Rex/Control/DTO/Creational/Builder/OverTime.cs
+++ Student Homework/Rex/Control/DTO/Creational/Builder/OverTime.cs
@@ -11,8 +11,9 @@ namespace hsdc.dpt.Control.DTO.Creational.Builder
         public string Applier { get; set; }
 
         [Required]
-        public DateTime ApplyDateTime { get; set; }
+        public DateTime ApplyDateTime { get; set; } = DateTime.Now.Date;
 
+        [Range(1, 8, ErrorMessage = "最小為 1 小時 ， 最大 8 小時")]
         public int OverTimeHour { get; set; } = 1;
     }
 }
