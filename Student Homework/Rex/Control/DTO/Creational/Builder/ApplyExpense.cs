d607c30414da49d36bbccc855f1ebfff65f05994
rexlucien <rexlucien@hotmail.com>
2015-11-27 22:27:13 +0800

    Add OverTime  Action(Http.Get)

--- Student Homework/Rex/Control/DTO/Creational/Builder/ApplyExpense.cs
+++ Student Homework/Rex/Control/DTO/Creational/Builder/ApplyExpense.cs
@@ -3,15 +3,6 @@ using System.Collections.Generic;
 
 namespace hsdc.dpt.Control.DTO.Creational.Builder
 {
-    public class Leave
-    {
-        public String LeaveID { get; set; }
-        public String Applier { get; set; }
-        public String LeaveType { get; set; }
-        public int LeaveDay { get; set; }
-        public DateTime LeaveBeginDate { get; set; }
-    }
-
     public class ApplyExpense
     {
         public String ApplyExpenseID { get; set; }
@@ -32,12 +23,5 @@ namespace hsdc.dpt.Control.DTO.Creational.Builder
         public DateTime ApplyDate { get; set; }
         public List<ApplyExpenseDetail> LineItems { get; set; }
         public string Detail { get; set; }
-    }
-
-    public class ApplyExpenseDetail
-    {
-        public String ReferenceNumber { get; set; }
-        public DateTime OccureDate { get; set; }
-        public decimal Expense { get; set; }
     }
 }
---
+++ Student Homework/Rex/Control/DTO/Creational/Builder/OverTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hsdc.dpt.Control.DTO.Creational.Builder
+{
+    public class OverTime
+    {
+        public string OverTimeId { get; set; }
+
+        public string Applier { get; set; }
+
+        public DateTime ApplyDateTime { get; set; }
+
+        public int OverTimeHour { get; set; }
+    }
+}
